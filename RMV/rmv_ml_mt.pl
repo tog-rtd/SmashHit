10fa3e4370381d59bc554ce8fd06a4554c766eb4
Rance DeLong <r.delong@opengroup.org>
2021-11-04 14:49:48 -0700

    added AUDIT COM EPP NGAC and PRIV

--- RMV/rmv_ml_mt.pl
+++
@@ -1,8 +0,0 @@
-% RMV - Monitor Library - Model Templates
-% Work in Progress
-
-:- module(rmv_ml_mt,[
-	       ]).
-
-:- use_module('COM/param').
-
--- RMV/rmv_ml_pst.pl
+++
@@ -1,8 +0,0 @@
-% RMV - Monitor Library - Property Specification Templates
-% Work in Progress
-
-:- module(rmv_ml_pst,[
-	       ]).
-
-:- use_module('COM/param').
-
